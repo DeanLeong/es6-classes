36e42fab57254c8d4a4fc755afb08080fb8c8b61
Axylos <robertdraketalley@gmail.com>
2017-11-04 17:23:47 -0400

    triangle.js solution

--- classes_lab/src/triangle.js
+++ classes_lab/src/triangle.js
@@ -2,6 +2,12 @@ class Triangle extends Shape {
   constructor(a, b, c) {
     // a, b, and c are the three side lengths
     // all you need to do is call super with the right argument(s)
+    
+    // by viewing the beginning of the test file
+    // we see that the Shape constructor takes an array
+    // of side lengths, so wrapping the given arguments a, b, and c
+    // in an array and passing it to super is sufficient
+    super([a, b, c]);
   }
 
   // https://en.wikipedia.org/wiki/Heron%27s_formula#Formulation
@@ -11,7 +17,7 @@ class Triangle extends Shape {
   }
 
   get area () {
-                                 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_operator
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_operator
     return Triangle.heronFormula(...this.sideLengths);
   }
 }
