ac229845a95fa380a6d9e5c4cd2d8f0f91e47c9f
Axylos <robertdraketalley@gmail.com>
2017-11-04 22:06:09 -0400

    Square.js solution

--- classes_lab/src/square.js
+++ classes_lab/src/square.js
@@ -1,2 +1,12 @@
 // define the Square class here
 // remember to extend from Rectangle
+
+// start off by defining the Square class
+// like the other shapes
+class Square extends Rectangle {
+
+  constructor(sideLength) {
+    // recall that the constructor for Rectangle takes a length and a height parameter
+    super(sideLength, sideLength);
+  }
+}
